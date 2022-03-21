d83cb21676100b1790399e7fa9a643f2f46bed74
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-03-21 08:12:40 +0530

    My day1 assignments

---
+++ javaassignment/src/javaassignment/staticvariable.java
@@ -0,0 +1,22 @@
+package javaassignment;
+
+public class staticvariable {
+	static void incr() {
+ int a = 0; //static variable
+	System.out.println("choose it from static method");
+a=a+50;
+System.out.println("a value :" +a);
+}
+	static {   //static method
+	System.out.println("From static block");
+	 int a = 10;
+	} 
+	public static void main(String[]args)  //static block
+	{
+		System.out.println("from main block");
+		int a = 0;
+		System.out.println("value of a:" +a);
+		incr();
+		
+	}
+}
